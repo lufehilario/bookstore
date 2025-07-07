30429e24d32e9db2b6d67846f79b0b67f3c44591
lufehilario <lufe.hilario@Gmail.com>
2025-07-07 18:48:37 -0300

    correção exercicio

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -8,8 +8,8 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    #permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
